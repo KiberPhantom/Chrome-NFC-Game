d6dd160449b6f863d32636578c48c3ec7b275e0d
KiberPhantom <100571463+KiberPhantom@users.noreply.github.com>
2024-02-05 20:43:15 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 document.addEventListener("DOMContentLoaded", async () => {
-  alert("User clicked scan button");
+  const characterContainer = document.getElementById("character-container");
+  const character = document.getElementById("character");
+  const candy = document.getElementById("candy");
+  
+  character.style.animation = "idle 1s infinite";
 
   try {
     const ndef = new NDEFReader();
@@ -11,10 +15,26 @@ document.addEventListener("DOMContentLoaded", async () => {
     });
 
     ndef.addEventListener("reading", ({ message, serialNumber }) => {
+	  character.style.animation = "none";
+	  character.style.animation = "eat 1s forwards";
+	  spawnCandy();
       alert(`> Serial Number: ${serialNumber}`);
       alert(`> Records: (${message.records.length})`);
     });
   } catch (error) {
     alert("Argh! " + error);
   }
+  function spawnCandy() {
+      candy.style.display = "block";
+
+      // Animate candy towards the character
+      candy.style.animation = "candy-fall 2s linear forwards";
+
+      // After animation, hide candy
+      setTimeout(() => {
+        candy.style.display = "none";
+        // Reset character animation to idle
+        character.style.animation = "idle 1s infinite";
+      }, 2000);
+    }
 });
