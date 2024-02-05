52bf29eabfedaec6a1bc0ae0c2da40cb5d70d1e2
KiberPhantom <100571463+KiberPhantom@users.noreply.github.com>
2024-02-05 20:38:38 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,44 +1,20 @@
-if ('NDEFReader' in window) {
-  document.addEventListener("DOMContentLoaded", async () => {
-    const characterContainer = document.getElementById("character-container");
-    const character = document.getElementById("character");
-    const candy = document.getElementById("candy");
+document.addEventListener("DOMContentLoaded", async () => {
+  alert("User clicked scan button");
 
-    // Play idle animation initially
-    character.style.animation = "idle 1s infinite";
+  try {
+    const ndef = new NDEFReader();
+    await ndef.scan();
+    alert("> Scan started");
 
-    try {
-      const ndef = new NDEFReader();
-      await ndef.scan();
+    ndef.addEventListener("readingerror", () => {
+      alert("Argh! Cannot read data from the NFC tag. Try another one?");
+    });
 
-      ndef.addEventListener("readingerror", () => {
-        // Обработка ошибок
-      });
-
-      ndef.addEventListener("reading", ({ message, serialNumber }) => {
-        character.style.animation = "none";
-        character.style.animation = "eat 1s forwards";
-        spawnCandy();
-      });
-    } catch (error) {
-      // Обработка ошибок
-    }
-
-    // Function to spawn candy and animate it towards the character
-    function spawnCandy() {
-      candy.style.display = "block";
-
-      // Animate candy towards the character
-      candy.style.animation = "candy-fall 2s linear forwards";
-
-      // After animation, hide candy
-      setTimeout(() => {
-        candy.style.display = "none";
-        // Reset character animation to idle
-        character.style.animation = "idle 1s infinite";
-      }, 2000);
-    }
-  });
-} else {
-  alert("No no no");
-}
+    ndef.addEventListener("reading", ({ message, serialNumber }) => {
+      alert(`> Serial Number: ${serialNumber}`);
+      alert(`> Records: (${message.records.length})`);
+    });
+  } catch (error) {
+    alert("Argh! " + error);
+  }
+});
